b653714eefda378d97fed8eec68b2ea409e81d45
Berta Floran <bertafloran@hotmail.com>
2022-08-01 20:38:08 +0200

    fix package Json

--- src/components/Button/ButtonStyled.ts
+++ src/components/Button/ButtonStyled.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 const ButtonStyled = styled.button`
-  font-size: 15px;
+  font-size: 1rem;
   text-align: center;
   color: #fff;
-  border-radius: 50px;
+  border-radius: 3rem;
   border: none;
-  width: 80px;
-  height: 40px;
+  width: 5rem;
+  height: 3rem;
   background-color: #ff6b00;
   font-weight: bold;
 `;
