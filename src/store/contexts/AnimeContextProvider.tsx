1855895be244f82fef21cd99804e686993a2300e
Berta Floran <bertafloran@hotmail.com>
2022-08-02 16:15:04 +0200

    get info from remote anime api

--- src/store/contexts/AnimeContextProvider.tsx
+++ src/store/contexts/AnimeContextProvider.tsx
@@ -1,28 +1,43 @@
+import { useEffect, useState } from "react";
 import { AnimeContext, IAnimeContext } from "./AnimeContext";
 
 interface AnimeContextProviderProps {
   children: JSX.Element | JSX.Element[];
 }
 
-const AnimeInfoTest: IAnimeContext = {
-  animeInfo: {
-    pagination: {
-      has_next_page: false,
-      current_page: 1,
-      items: {
-        count: 0,
-        total: 666,
-      },
-    },
-    data: [],
-  },
-};
-
 const AnimeContextProvider = ({
   children,
 }: AnimeContextProviderProps): JSX.Element => {
+  const animeInfoTest: IAnimeContext = {
+    animeInfo: {
+      pagination: {
+        has_next_page: false,
+        current_page: 1,
+        items: {
+          count: 0,
+          total: 666,
+        },
+      },
+      data: [],
+    },
+  };
+
+  const [animeInfo, setAnimeInfo] = useState(animeInfoTest.animeInfo);
+
+  const apiURL = "https://api.jikan.moe/v4/top/anime?limit=10";
+
+  const loadAnimeApi = async () => {
+    const response = await fetch(apiURL);
+    const animeApiInfo = await response.json();
+    setAnimeInfo(animeApiInfo);
+  };
+
+  useEffect(() => {
+    loadAnimeApi();
+  }, []);
+
   return (
-    <AnimeContext.Provider value={AnimeInfoTest}>
+    <AnimeContext.Provider value={{ animeInfo }}>
       {children}
     </AnimeContext.Provider>
   );
