2dc7cce9fcc68faefeda597ea6c017e1d865b253
David Lozano <dawgun@hotmail.com>
2022-08-03 19:33:45 +0200

    created UIReducer test

---
+++ src/store/reducers/UIReducer/UIReducer.test.ts
@@ -0,0 +1,66 @@
+import UIReducer from "./UIReducer";
+
+describe("Given the UIReducer function", () => {
+  describe("When it's instantiated with an object with property isLoading 'false' and showLoading Action", () => {
+    test("Then should return an object with isLoading 'true'", () => {
+      const loadingState = {
+        isLoading: false,
+      };
+      const showLoadingAction = {
+        type: "showLoading",
+      };
+      const newloadingState = {
+        isLoading: true,
+      };
+
+      const showLoadingCreatonAction = UIReducer(
+        loadingState,
+        showLoadingAction
+      );
+
+      expect(showLoadingCreatonAction).toEqual(newloadingState);
+    });
+  });
+
+  describe("When it's instantiated with an object with property isLoading 'true' and closeLoading Action", () => {
+    test("Then should return an object with isLoading 'false'", () => {
+      const loadingState = {
+        isLoading: true,
+      };
+      const showLoadingAction = {
+        type: "closeLoading",
+      };
+      const newloadingState = {
+        isLoading: false,
+      };
+
+      const showLoadingCreatonAction = UIReducer(
+        loadingState,
+        showLoadingAction
+      );
+
+      expect(showLoadingCreatonAction).toEqual(newloadingState);
+    });
+  });
+
+  describe("When it's instantiated with an object with property isLoading 'true' and 'Testing' Action", () => {
+    test("Then should return an object with isLoading 'true'", () => {
+      const loadingState = {
+        isLoading: true,
+      };
+      const showLoadingAction = {
+        type: "Testing",
+      };
+      const newloadingState = {
+        isLoading: true,
+      };
+
+      const showLoadingCreatonAction = UIReducer(
+        loadingState,
+        showLoadingAction
+      );
+
+      expect(showLoadingCreatonAction).toEqual(newloadingState);
+    });
+  });
+});
