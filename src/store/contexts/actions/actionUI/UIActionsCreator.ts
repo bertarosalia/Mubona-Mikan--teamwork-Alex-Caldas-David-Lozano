3f802d4e30562600106533a35b04be8c7f2ade32
David Lozano <dawgun@hotmail.com>
2022-08-03 16:59:24 +0200

    created UIActionCreator

---
+++ src/store/contexts/actions/actionUI/UIActionsCreator.ts
@@ -0,0 +1,12 @@
+import {
+  CloseLoadingUI,
+  ShowLoadingUI,
+} from "../../../../types/actionTypesUI/actionsUI";
+
+export const showLoadingActionCreator = (): ShowLoadingUI => ({
+  type: "showLoading",
+});
+
+export const closeLoadingActionCreator = (): CloseLoadingUI => ({
+  type: "closeLoading",
+});
