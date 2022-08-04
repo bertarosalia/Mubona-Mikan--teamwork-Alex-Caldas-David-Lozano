346ad9cbe6ffe1a04f7e7e51466991e9269ce104
Berta Floran <bertafloran@hotmail.com>
2022-08-04 17:16:59 +0200

    create not found page and its style

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import NotFoundPageStyled from "./NotFoundPageStyled";
+
+const NotFoundPage = (): JSX.Element => {
+  return (
+    <NotFoundPageStyled>
+      <h2 className="not-found-page">404 ERROR</h2>
+    </NotFoundPageStyled>
+  );
+};
+
+export default NotFoundPage;
