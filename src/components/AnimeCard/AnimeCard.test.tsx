2ae31f6f797fb86e59c53c483172175cdd0cc4c1
David Lozano <dawgun@hotmail.com>
2022-08-07 20:06:02 +0200

    deleted unused imports

--- src/components/AnimeCard/AnimeCard.test.tsx
+++ src/components/AnimeCard/AnimeCard.test.tsx
@@ -1,7 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import useAPI from "../../hooks/useAPI";
-import { UIContext } from "../../store/contexts/UIContext/UIContext";
 import AnimeCard from "./AnimeCard";
 
 describe("Given an AnimeCard component", () => {
