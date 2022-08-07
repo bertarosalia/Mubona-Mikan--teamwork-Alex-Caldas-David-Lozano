25004e769bcb734195e92c0d92b8efa1212606c4
Alex Caldas <alexcaldasaixarch@gmail.com>
2022-08-07 20:24:09 +0200

    fixed css conflict betwen pages and components

--- src/components/MyAnimeList/MyAnimeListStyled.ts
+++ src/components/MyAnimeList/MyAnimeListStyled.ts
@@ -1,16 +1,13 @@
 import styled from "styled-components";
 
 const MyAnimeListStyled = styled.main`
-  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   padding: 1rem;
+  min-height: 10rem;
 
-  .my-anime-list__title {
-    font-size: 2em;
-    color: #e5c185;
-  }
   .button_add_new {
+    z-index: -1;
     position: absolute;
     top: 128px;
     right: 10px;
@@ -20,6 +17,20 @@ const MyAnimeListStyled = styled.main`
     color: #e5c185;
     background-color: transparent;
     cursor: pointer;
+  }
+  .my-anime-list {
+    &__title {
+      font-size: 2rem;
+      color: #e5c185;
+    }
+
+    &__container {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      gap: 2rem;
+    }
   }
 `;
 
--- src/pages/MyListPage/MyListPage.tsx
+++ src/pages/MyListPage/MyListPage.tsx
@@ -1,16 +1,6 @@
 import MyAnimeList from "../../components/MyAnimeList/MyAnimeList";
-import MyAnimeListStyled from "../../components/MyAnimeList/MyAnimeListStyled";
 const MyListPage = (): JSX.Element => {
-  return (
-    <>
-      <MyAnimeListStyled>
-        <h2 className="my-anime-list__title">My Anime List</h2>
-        <button className="button_add_new">+</button>
-
-        <MyAnimeList />
-      </MyAnimeListStyled>
-    </>
-  );
+  return <MyAnimeList />;
 };
 
 export default MyListPage;
